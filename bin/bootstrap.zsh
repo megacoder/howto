c9e5795e3ee7b6351803ceeeac4b332e0e1afbf5
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2014-05-24 18:56:41 -0400

    Cleanup

--- bin/bootstrap.zsh
+++ bin/bootstrap.zsh
@@ -13,19 +13,13 @@ fi
 echo "Cleaning up any prior configuration."
 find . -name config.cache -print | xargs rm -f
 find . -name autom4te.cache -print | xargs rm -rf
-if [[ -x ${VPATH}/bootstrap.sh ]]; then
-	echo "... ${VPATH}/via bootstrap.sh"
-	${VPATH}/bootstrap.sh --help
-elif [[ -x ${VPATH}/autogen.sh ]]; then
-	echo "... via ${VPATH}/autogen.sh"
-	${VPATH}/autogen.sh --help
-elif [[ -x ${VPATH}/bootstrap ]]; then
-	echo "... via ${VPATH}/bootstrap"
-	${VPATH}/bootstrap --help
-else
-	echo "... via autoreconf"
-	autoreconf -fvi ${VPATH}
-fi
+tool="autoreconf -fvim ${VPATH}"
+for trial in bootstrap{,.{zsh,sh}}; do
+	if [[ -x "${VPATH}/${trial}" ]]; then
+		tool="${VPATH}/${trial}"
+	fi
+done
+eval ${tool}
 if [[ ! -x ./configure ]]; then
 	echo "Could not find or produce a ./configure file!"
 	exit 1
