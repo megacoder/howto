db1c157ee1a807f1c389d4acec0e6d31e203ea37
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2017-03-03 11:31:40 -0600

    Reduced complexity of "bootstrap"

--- bin/bootstrap.zsh
+++ bin/bootstrap.zsh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-[[ "${BOOTSTRAP_VERBOSE}" = "" ]] || set -x
+
 ME=${0:t:r}
 
 VPATH=.
@@ -10,35 +10,22 @@ if [[ ! -d "${VPATH}" ]]; then
 	echo "${ME}: VPATH (${VPATH}) does not exist." >&2
 	exit 1
 fi
-echo "Cleaning up any prior configuration."
-for name in config.cache autom4te.cache; do
-	find . -name "${name}" -exec rm -rf {} + -print
-done
-tool="autoreconf -fvim ${VPATH}"
-for trial in bootstrap{,.{zsh,sh}}; do
-	if [[ -x "${VPATH}/${trial}" ]]; then
-		tool="${VPATH}/${trial}"
-	fi
-done
-eval ${tool}
-if [[ ! -x ./configure ]]; then
-	echo "Could not find or produce a ./configure file!"
+export VPATH
+
+if [[ -f configure.ac ]] || [[ -f configure.in ]]; then
+	echo "${ME}: Running aclocal..."
+	aclocal  -I m4 --install
+	echo "${ME}: Running autoconf..."
+	autoconf -I m4 --force
+fi
+
+if [[ -f Makefile.am ]]; then
+	echo "${ME}: Running automake..."
+	automake --add-missing --copy --force-missing
+fi
+
+if [[ ! -x configure ]]; then
+	echo "${ME}: did not produce a ./configure script." >&2
 	exit 1
 fi
-echo "Running configure with default arguments"
-export	CC="gcc -std=gnu99 -march=native"
-export	CFLAGS="${CFLAGS} -pipe -Os"
-export	CXX="g++ -march=native"
-export	CXXFLAGS="${CXXFLAGS} -pipe -Os"
-if [[ /bin/pump ]]; then
-	eval $(/bin/pump --startup)
-	ZSHEXT()	{
-		/bin/pump --shutdown
-	}
-fi
-./configure								\
-	--enable-silent-rules						\
-	"$@"
-if [[ -f Makefile ]]; then
-	[[ -x /bin/pump ]] && pump make -j20 || make -j20
-fi
+exit 0
