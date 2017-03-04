fed9a14625cfe6e2c9b2c2e4d93830eace17cdad
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2017-03-04 17:29:07 -0600

    Run libtoolize(1) if our configure.ac file mentions LT_INIT

--- bin/bootstrap.zsh
+++ bin/bootstrap.zsh
@@ -12,15 +12,27 @@ if [[ ! -d "${VPATH}" ]]; then
 fi
 export VPATH
 
-if [[ -f configure.ac ]] || [[ -f configure.in ]]; then
-	echo "${ME}: Running aclocal..."
+CONFIG=
+for config in configure.ac configure.in; do
+	if [[ -f "${config}" ]]; then
+		CONFIG="${config}"
+		break
+	fi
+done
+
+if [[ ! -z "${CONFIG}" ]]; then
+	if [[ -x /bin/libtoolize ]] && fgrep -q -s LT_INIT "${CONFIG}" ; then
+		echo "${ME}: running libtoolize ..."
+		/bin/libtoolize -f -i -q
+	fi
+	echo "${ME}: Running aclocal ..."
 	aclocal  -I m4 --install
-	echo "${ME}: Running autoconf..."
+	echo "${ME}: Running autoconf ..."
 	autoconf -I m4 --force
 fi
 
 if [[ -f Makefile.am ]]; then
-	echo "${ME}: Running automake..."
+	echo "${ME}: Running automake ..."
 	automake --add-missing --copy --force-missing
 fi
 
